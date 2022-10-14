a7732791e571405fc78c3954672d0cbd58bf28ca
Joseph Chu <karkinoscode@gmail.com>
2022-10-14 17:36:37 -0400

    Finished the progress bar functionality

--- script.js
+++ script.js
@@ -85,7 +85,6 @@ function updateProgressBar(e) {
     progress.style.width = `${progressPercent}%`;
     // Calculate Display for Duration
     const durationMinutes = Math.floor(duration / 60);
-    console.log("minutes", durationMinutes);
     let durationSeconds = Math.floor(duration % 60);
     if (durationSeconds < 10) {
       durationSeconds = `0${durationSeconds}`;
@@ -96,17 +95,25 @@ function updateProgressBar(e) {
     }
     // Calculate Display for currentTime
     const currentMinutes = Math.floor(currentTime / 60);
-    console.log("minutes", currentMinutes);
     let currentSeconds = Math.floor(currentTime % 60);
     if (currentSeconds < 10) {
       currentSeconds = `0${currentSeconds}`;
     }
-    console.log("seconds", currentSeconds);
     currentTimeEl.textContent = `${currentMinutes}:${currentSeconds}`;
   }
+}
+
+// Set Progress Bar
+function setProgressBar(e) {
+  const width = this.clientWidth;
+  const clickX = e.offsetX;
+  const { duration } = music;
+  music.currentTime = (clickX / width) * duration;
 }
 
 // Event Listeners
 prevBtn.addEventListener("click", prevSong);
 nextBtn.addEventListener("click", nextSong);
 music.addEventListener("timeupdate", updateProgressBar);
+music.addEventListener("ended", nextSong);
+progressContainer.addEventListener("click", setProgressBar);
